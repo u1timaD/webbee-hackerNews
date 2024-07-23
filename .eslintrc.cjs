f2b035d9c8af03b2260032b4b456bda50aa9dc7f
Dmitry <free-doom46@mail.ru>
2024-07-23 18:34:20 +0300

    deps(hackernews): настраивает зависимости

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:@web-bee-ru/base', 'plugin:@web-bee-ru/react',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
