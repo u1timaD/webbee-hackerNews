095cfd223f8df3a73d32491f953d4a5906b396e5
Dmitry <free-doom46@mail.ru>
2024-07-26 15:53:04 +0300

    deps(hackernews): устанавливает зависимости

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,16 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'plugin:@web-bee-ru/base', 'plugin:@web-bee-ru/react',
+    'plugin:@web-bee-ru/base',
+    'plugin:@web-bee-ru/react',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
